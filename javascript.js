d15f638ee597ffb07e5f073e8e0acc77cd84f5b4
Daniel <191582347+danhoernchen@users.noreply.github.com>
2024-12-15 19:56:30 +0100

    grid, controls
    forgot about the commit lessons (commit often, atomic commits) so there is a whole lot in this one.
    added the grid, the logic to make the color-change, the controls for color and grid size.

---
+++ javascript.js
@@ -0,0 +1,31 @@
+const mainContainer = document.getElementById("mainContainer");
+const color1 = document.getElementById("color1");
+const color2 = document.getElementById("color2");
+const resetBtn = document.getElementById("resetBtn");
+const sizeInput = document.getElementById("size");
+resetBtn.addEventListener("click", createGrid);
+
+function resetGrid() {}
+
+function createGrid() {
+  mainContainer.innerHTML = "";
+  for (i = 0; i < sizeInput.value; i++) {
+    for (current = 0; current < sizeInput.value; current++) {
+      const gridContainer = document.createElement("div");
+      gridContainer.classList.add("gridDiv");
+      const width = 100 / sizeInput.value;
+      gridContainer.style.width = `${width}%`;
+      gridContainer.style.height = `${width}%`;
+      gridContainer.style.backgroundColor = color1.value;
+      gridContainer.addEventListener("mouseover", changeColor);
+      mainContainer.appendChild(gridContainer);
+    }
+  }
+}
+
+function changeColor() {
+  this.style.backgroundColor = color2.value;
+  console.log("changeColor");
+}
+
+document.addEventListener("DOMContentLoaded", createGrid);
